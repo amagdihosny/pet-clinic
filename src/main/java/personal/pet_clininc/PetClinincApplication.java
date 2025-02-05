508abb699b45b8c75e6ab3ed6d70f5809551eb46
amagdi <ahmed.magdi.hosny@gmail.com>
2025-02-05 18:15:35 +0200

    initial project

---
+++ src/main/java/personal/pet_clininc/PetClinincApplication.java
@@ -0,0 +1,13 @@
+package personal.pet_clininc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinincApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/pet_clininc/PetClinincApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.pet_clininc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinincApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
